055eb4db86e0c0d378db16434546973ef8634ea8
leandro.iglezias <leeiglezias@gmail.com>
2024-12-06 09:22:36 -0300

    Initial commit

---
+++ src/main/java/lnaix/estudos/santander_2023/Santander2023Application.java
@@ -0,0 +1,13 @@
+package lnaix.estudos.santander_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/lnaix/estudos/santander_2023/Santander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package lnaix.estudos.santander_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
