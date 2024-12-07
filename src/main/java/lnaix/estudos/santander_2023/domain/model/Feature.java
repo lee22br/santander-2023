723082fe3696fbc73c25509eb82e6a3f0d353501
leandro.iglezias <leeiglezias@gmail.com>
2024-12-07 10:35:14 -0300

    Domains Criados

--- src/main/java/lnaix/estudos/santander_2023/domain/model/Feature.java
+++ src/main/java/lnaix/estudos/santander_2023/domain/model/Feature.java
@@ -1,9 +1,6 @@
 package lnaix.estudos.santander_2023.domain.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity(name = "tb_feature")
 public class Feature extends BaseItem {
